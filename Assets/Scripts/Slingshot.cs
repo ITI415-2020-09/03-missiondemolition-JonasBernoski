df590dfbe9b39f0783c10d2da509766aff272f9e
Jonas Bernoski <jonas2015@aol.com>
2020-09-23 14:38:42 -0400

    Highlight

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject Launchpoint;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Transform LaunchpointTrans = transform.Find("Launchpoint");
+        Launchpoint = LaunchpointTrans.gameObject;
+        Launchpoint.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        Launchpoint.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnMouseExit()
+    {
+        // print("Slingshot:OnMouseExit()"); 
+        Launchpoint.SetActive(false);
+    }
+}
