01743fafb8e3ebc756b12a9f3a8efe1f8cbc4416
Jonas Bernoski <jonas2015@aol.com>
2020-09-23 20:30:24 -0400

    Long Code

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -15,9 +17,19 @@ public class Slingshot : MonoBehaviour
     public bool aimingMode;
 
     private Rigidbody projectileRigidbody;
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.LaunchPos;
+        }
+    }
     // Start is called before the first frame update
     void Awake()
     {
+        S = this;
         Transform LaunchpointTrans = transform.Find("Launchpoint");
         Launchpoint = LaunchpointTrans.gameObject;
         Launchpoint.SetActive(false);
