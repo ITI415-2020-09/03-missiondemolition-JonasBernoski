1f216a923e313442b1f1b9340d6a3ad86b131fd1
Jonas Bernoski <jonas2015@aol.com>
2020-09-23 21:37:33 -0400

    Finished

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -83,6 +83,8 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 
